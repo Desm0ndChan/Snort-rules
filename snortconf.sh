02f4890769aaa43f59ad57c25e75a3e03ca20b48
Desmond Chan <desm0nd@desm0ndCh4n.local>
2023-05-04 19:33:51 +0800

    added validation script

---
+++ snortconf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+$INTERFACE="enp0s8"
+$ALARM_MODE="console"
+
+# Check if the promisc mode enabled
+/usr/sbin/ip link show $INTERFACE | /usr/bin/grep -q promisc && /usr/bin/echo "Promiscuous mode is enabled" || /usr/bin/echo "Promiscuous mode is disabled"
+if [ $? -ne 0 ] ; then
+    echo "Promiscuous mode is disabled, enabling now"
+    /usr/sbin/ip link set $INTERFACE promisc on
+fi
+
+echo "Checking the config file"
+
+# Check if the config file exists
+if [ ! -e /etc/snort/snort.conf ] ; then
+    echo "No Snort config file found!"
+    exit 1
+fi
+# Check if the config file is valid
+/usr/sbin/snort -T -i $INTERFACE -c /etc/snort/snort.conf
+if [ $? -ne 0 ] ; then
+    echo "Snort exited with an error!"
+    exit 1
+fi
+echo "Done"
+
+echo "Starting Snort"
+/usr/sbin/snort -A $ALARM_MODE -i $INTERFACE -c /etc/snort/snort.conf
